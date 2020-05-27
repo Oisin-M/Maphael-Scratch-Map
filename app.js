0083a2ccd9e9a3e5cc2d40307dbc31948e6876b1
Oisin-M <oisinmmm@gmail.com>
2020-05-27 11:55:34 +0100

    works, able to implement anything now. Decision time now

--- app.js
+++ app.js
@@ -1,11 +1,45 @@
-var getTooltip = function(elem) {
+const getTooltip = (elem) => {
     // Show element ID
     console.log(elem);
-    return $(elem.node).attr("data-id");
+    return $(elem.node).attr("data-id")+": Unvisited";
 };
 
 countries="FO,UM,US,JP,SC,IN,FR,FM,CN,PT,SW,SH,BR,KI,PH,MX,ES,BU,MV,SP,GB,GR,AS,DK,GL,GU,MP,PR,VI,CA,ST,CV,DM,NL,JM,WS,OM,VC,TR,BD,LC,NR,NO,KN,BH,TO,FI,ID,MU,SE,TT,MY,PA,PW,TV,MH,CL,TH,GD,EE,AG,TW,BB,IT,MT,VU,SG,CY,LK,KM,FJ,RU,VA,SM,KZ,AZ,TJ,LS,UZ,MA,CO,TL,TZ,AR,SA,PK,YE,AE,KE,PE,DO,HT,PG,AO,KH,VN,MZ,CR,BJ,NG,IR,SV,SL,GW,HR,BZ,ZA,CF,SD,CD,KW,DE,BE,IE,KP,KR,GY,HN,MM,GA,GQ,NI,LV,UG,MW,AM,SX,TM,ZM,NC,MR,DZ,LT,ET,ER,GH,SI,GT,BA,JO,SY,MC,AL,UY,CNM,MN,RW,SO,BO,CM,CG,EH,RS,ME,TG,LA,AF,UA,SK,JK,BG,QA,LI,AT,SZ,HU,RO,NE,LU,AD,CI,LR,BN,IQ,GE,GM,CH,TD,KV,LB,DJ,BI,SR,IL,ML,SN,GN,ZW,PL,MK,PY,BY,CZ,BF,NA,LY,TN,BT,MD,SS,BW,BS,NZ,CU,EC,AU,VE,SB,MG,IS,EG,KG,NP";
 
+
+const OnClick=(e, id, mapElem, textElem) => {
+    var newData = {
+        'areas': {}
+    };
+
+    console.log(e);
+    console.log(id);
+    console.log(mapElem);
+    console.log(textElem);
+
+    if (mapElem.originalAttrs.fill == "#5ba4ff") {
+        console.log("click");
+        newData.areas[id] = {
+            attrs: {
+                fill: "#0088db"
+            },
+            tooltip: {
+                content: id+": Visited"
+            }
+        };
+    } else {
+        console.log("unclick");
+        newData.areas[id] = {
+            attrs: {
+                fill: "#5ba4ff"
+            },
+            tooltip: {
+                content: id+": Unvisited"
+            }
+        };
+    }
+    $(".mapcontainer").trigger('update', [{mapOptions: newData}]);
+}
 
 //need to add custom data and legend to go anywhere further. get country list from world_countries_miller
 //just make visited 0 or 1 and follow the legend tutorials
@@ -20,7 +54,8 @@ $(function () {
               attrs: {
                   fill: "#5ba4ff",
                   stroke: "#99c7ff",
-                  cursor: "pointer"
+                  cursor: "pointer",
+                  "stroke-width": "0.25"
               },
               tooltip: {
                   content: getTooltip
@@ -39,27 +74,7 @@ $(function () {
                   }
               },
               eventHandlers: {
-                  click: function (e, id, mapElem, textElem) {
-                      var newData = {
-                          'areas': {}
-                      };
-                      if (mapElem.originalAttrs.fill == "#5ba4ff") {
-                          console.log("click");
-                          newData.areas[id] = {
-                              attrs: {
-                                  fill: "#0088db"
-                              }
-                          };
-                      } else {
-                          console.log("unclick");
-                          newData.areas[id] = {
-                              attrs: {
-                                  fill: "#5ba4ff"
-                              }
-                          };
-                      }
-                      $(".mapcontainer").trigger('update', [{mapOptions: newData}]);
-                  }
+                  click: OnClick
               }
           }
       },
